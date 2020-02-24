a29e7161ec51ff52e0d450c8f068d08ac95d8825
shuta13 <hollystarsun@gmail.com>
2020-02-25 02:27:49 +0900

    chore: html with webpack

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,16 +1,34 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const SRC_PATH    = path.resolve(__dirname, "../src");
 const PUBLIC_PATH = path.resolve(__dirname, "../public");
 
 module.exports = {
   entry: {
-    main: SRC_PATH + "/index.ts"
+    index: SRC_PATH + "/index.ts",
+    contents: SRC_PATH + "/components/pages/contents.ts"
   },
   output: {
     filename: "[name].js",
-    path: PUBLIC_PATH + "/dist/js/"
+    path: PUBLIC_PATH
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "garelly",
+      inject: true,
+      chunks: ["index"],
+      filename: "index.html",
+      template: "src/components/pages/index.html"
+    }),
+    new HtmlWebpackPlugin({
+      title: "garelly - contents",
+      inject: true,
+      chunks: ["contents"],
+      filename: "contents.html",
+      template: "src/components/pages/contents.html"
+    })
+  ],
   module: {
     rules: [
       {
