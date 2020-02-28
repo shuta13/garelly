13af9106a73d3ea2ae0206bed905938aa4878ff7
shuta13 <hollystarsun@gmail.com>
2020-02-28 21:00:15 +0900

    fix: class

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,32 +2,33 @@ import "./index.scss";
 import "../components/common/AppImage/AppImage.scss";
 
 const css = new CSSStyleSheet() as any;
-css.replace(`
-  @import "index.css"
-`);
 
 class AppImage extends HTMLElement {
   constructor() {
     super();
+    this.createDOMTree();
+  }
 
-    // new shadow root
-    const shadow = this.attachShadow({ mode: "open" });
-
-    // adopt stylesheet
-    const shadowRoot = this.shadowRoot as any;
-    shadowRoot.adoptedStyleSheets = [css];
-
-    // make shadow dom tree
+  createDOMTree() {
     const appImageWrap = document.createElement("div");
     appImageWrap.setAttribute("class", "AppImageWrap");
     const appImageInfo = document.createElement("div");
     appImageInfo.setAttribute("class", "AppImageInfo");
     appImageInfo.textContent = "this is app-image";
     appImageWrap.appendChild(appImageInfo);
+    this.adaptStyleSheet(appImageWrap);
+  };
 
-    // append shadow dom tree
-    shadow.appendChild(appImageWrap);
-  }
+  adaptStyleSheet(appImageWrap: HTMLDivElement) {
+    // new shadow root
+    const shadow = this.attachShadow({ mode: "open" });
+    // adopt stylesheet
+    const shadowRoot = this.shadowRoot as any;
+    css.replace(`@import "index.css";`).then(() => {
+      shadow.appendChild(appImageWrap);
+    });
+    shadowRoot.adoptedStyleSheets = [css];
+  };
 }
 
 customElements.define("app-image", AppImage);
