d1e5ab5ec222a85947615b71150df0af0a8827fa
shuta13 <hollystarsun@gmail.com>
2020-02-29 23:31:02 +0900

    setup

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const workboxPlugin = require("workbox-webpack-plugin");
@@ -46,8 +45,7 @@ module.exports = {
       swDest: PUBLIC_PATH + "/sw.js",
       clientsClaim: true,
       skipWaiting: true,
-    }),
-    new CleanWebpackPlugin(),
+    })
   ],
   module: {
     rules: [
