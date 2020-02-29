7eb903fc2494e80f809243fc9089ae0200faddf0
shuta13 <hollystarsun@gmail.com>
2020-02-29 14:50:39 +0900

    feat: loading page

--- src/components/partials/Loading/Loading.ts
+++ src/components/partials/Loading/Loading.ts
@@ -25,6 +25,7 @@ export default class Loading extends HTMLElement {
     const LoadingStyle = document.createElement("style");
     LoadingStyle.textContent = `
       .LoadingWrap {
+        position: absolute;
         width: 100vw;
         height: 100vh;
         display: flex;
